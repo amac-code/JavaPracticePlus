1bf217efa1eb33bb807aa82069a27411ce7582a2
amac-code <dr.anshumanmac@outlook.com>
2023-09-27 00:21:25 +0530

    RightTriangle

--- src/com/patterns/RightTriangle.java
+++ src/com/patterns/RightTriangle.java
@@ -1,5 +1,49 @@
 package com.patterns;
 
+//Problem Statement:
+/* 
+ Write a Java program to print the following right triangle pattern:
+
+    *
+    * *
+    * * *
+    * * * *
+    * * * * *
+
+*/
+
 public class RightTriangle {
-    
+    public static void main(String[] args) {
+        int row = 5;
+        System.out.println();
+
+        for (int i = 1; i <= row; i++) {
+            
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
 }
+
+
+
+//Dry Run:
+/**
+ *  Dry Run of this program for row = 5:
+
+    Outer loop, i = 1:
+        Inner loop, j = 1: Prints * , result: * .
+        Move to the next line. 
+        Output so far: * .
+    
+    Outer loop, i = 2:
+        Inner loop, j = 1: Prints * , result: * .
+        Inner loop, j = 2: Prints * , result: * * .
+        Move to the next line.
+        Output so far: * * .
+
+    This pattern continues for i = 3, i = 4, and i = 5, with each row adding one more * to the output.
+ */
